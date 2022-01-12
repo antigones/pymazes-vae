0b7206782fc721d6084181653337a705ed0d780e
Juna Salviati <juna.salviati@gmail.com>
2022-01-12 19:20:15 +0100

    tape.gradients readded

--- maze_vae.py
+++ maze_vae.py
@@ -51,8 +51,8 @@ class VAE(keras.Model):
             kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
             kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
             total_loss = reconstruction_loss + kl_loss
-        # grads = tape.gradient(total_loss, self.trainable_weights)
-        # self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
         self.total_loss_tracker.update_state(total_loss)
         self.reconstruction_loss_tracker.update_state(reconstruction_loss)
         self.kl_loss_tracker.update_state(kl_loss)
